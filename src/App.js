ace4b05d9e3db470a7b3d9ccf42159d85967390d
Jacob Erickson <Jakerson8@gmail.com>
2022-08-17 17:04:01 -0600

    finsihed userboy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react';
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 function App() {
+  const[usersList,SetUsersList]=useState([]);
+  const addUserHandler =(uName,uAge)=>{
+    SetUsersList((prevUsersList)=>{
+      return [...prevUsersList,{name: uName, age: uAge}]
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+ <AddUser onAddUser ={addUserHandler}/>
+ <UserList users = {usersList}/>
     </div>
   );
 }
